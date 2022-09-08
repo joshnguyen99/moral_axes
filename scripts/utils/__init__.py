6db0021d6736ca978b3f6785671e73c89b1fc953
Josh Nguyen <josh@joshnguyen.net>
2022-09-08 18:49:44 +1000

    Add binary classifier evaluation example

--- scripts/utils/__init__.py
+++ scripts/utils/__init__.py
@@ -1,2 +1,3 @@
 from utils.create_concepts import make_one_concept
 from utils.create_concepts import make_concepts_from_lexicon
+from utils.evaluation import evaluate_binary_scorer_fold, mean_sd_ci, evaluate_binary_scorer
