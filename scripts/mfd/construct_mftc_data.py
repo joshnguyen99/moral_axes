a26f44346d65f00a7bc3cd0f37cc5c3d16040540
Josh Nguyen <tuandungnguyen99@gmail.com>
2022-11-05 03:32:00 +0000

    Add train-test splitting

--- scripts/mfd/construct_mftc_data.py
+++ scripts/mfd/construct_mftc_data.py
@@ -5,6 +5,8 @@ import argparse
 import os
 import warnings
 import json
+from sklearn.model_selection import StratifiedKFold, train_test_split
+import numpy as np
 
 nlp = spacy.load("en_core_web_md", disable=["ner", "senter"])
 
@@ -24,6 +26,10 @@ def parse_args():
                         type=int,
                         help="Number of cores to use for parallelization.",
                         default=-1)
+    parser.add_argument("--random_state",
+                        type=int,
+                        help="Random state for data spliting.",
+                        default=100)
     return parser.parse_args()
 
 
@@ -90,6 +96,77 @@ if __name__ == "__main__":
     mftc_df = pd.DataFrame(mftc_df, columns=["tweet"] + foundations)
     print(len(mftc_df))
     print("Finished processing the annotations.")
+
+    # Splitting into training and test sets
+    print("Splitting the data into training and test sets...")
+
+    foundations = ["authority", "care", "fairness", "loyalty", "sanctity", "none"]
+    for foundation in foundations:
+        if foundation == "none":
+            # Seen but not highlighted by anyone
+            y = mftc_df[["care", "authority", "fairness",
+                         "loyalty", "sanctity"]].max(1) == 0
+            y = y.astype(int)
+            mftc_df[f"{foundation}_label"] = 0
+            mftc_df.loc[y[y == 1].index, f"{foundation}_label"] = 1
+
+            y_train, y_test = train_test_split(mftc_df[f"{foundation}_label"],
+                                               test_size=0.1,
+                                               shuffle=True,
+                                               stratify=mftc_df[f"{foundation}_label"],
+                                               random_state=args.random_state)
+
+            mftc_df[f"{foundation}_train"] = 0
+            mftc_df.loc[y_train.index, f"{foundation}_train"] = 1
+        else:
+            pos = mftc_df[foundation] >= 1
+            neg = mftc_df[foundation] == 0
+            pos, neg = pos.astype(int), neg.astype(int)
+            # Label explanation:
+            # -1: the sentence has not been seen by anyone assigned this foundation
+            #  0: the sentence has been seen at least once with this foundation,
+            #     but was not highlighted
+            # +1: the sentence has been seen at least once with this foundation,
+            #     and was highlighted at least once
+            mftc_df[f"{foundation}_label"] = -1
+            mftc_df.loc[pos[pos == 1].index, f"{foundation}_label"] = 1
+            mftc_df.loc[neg[neg == 1].index, f"{foundation}_label"] = 0
+            y = mftc_df[(mftc_df[f"{foundation}_label"] == 0) |
+                        (mftc_df[f"{foundation}_label"] == 1)][f"{foundation}_label"]
+            y = y.astype(int)
+            y_train, y_test = train_test_split(y, test_size=0.1, shuffle=True,
+                                               stratify=y,
+                                               random_state=args.random_state)
+            mftc_df[f"{foundation}_train"] = -1
+            mftc_df.loc[y_train.index, f"{foundation}_train"] = 1
+            mftc_df.loc[y_test.index, f"{foundation}_train"] = 0
+
+    print("Finished train-test-split.")
+
+    print("Performing k-fold splitting on the training set...")
+
+    foundations = ["authority", "care", "fairness", "loyalty", "sanctity", "none"]
+    for f in foundations:
+        # Not seen => fold = -1
+        mftc_df[f + "_fold"] = -1
+
+        # Perform kfold split on the training set
+        index = mftc_df[mftc_df[f + "_label"] >= 0].index
+        index = np.array(index)
+        labels = mftc_df[mftc_df[f + "_label"] >= 0][f + "_label"]
+        skf = StratifiedKFold(n_splits=10, random_state=args.random_state, shuffle=True)
+        a = []
+        for fold_no, (train_idx, test_idx) in enumerate(skf.split(index, labels)):
+            idx = index[test_idx]
+            mftc_df.loc[idx, f + "_fold"] = fold_no + 1
+            a.extend(idx)
+        assert sorted(a) == sorted(index)
+
+        # Test => fold = 0
+        index_test = mftc_df[mftc_df[f + "_train"] == 0].index
+        mftc_df.loc[index_test, f + "_fold"] = 0
+
+    print("Finished k-fold splitting.")
 
     # Save
     mftc_df.index = range(len(mftc_df))
