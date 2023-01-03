aa4e572d6c03e66aa3afb00f3709da973293b6f9
Josh Nguyen <tuandungnguyen99@gmail.com>
2023-01-03 00:55:03 +0000

    Add __main__

--- scripts/score_mf_ddr.py
+++ scripts/score_mf_ddr.py
@@ -4,7 +4,10 @@ import pandas as pd
 import numpy as np
 from utils import make_concepts_from_lexicon
 from gensim.models import KeyedVectors
-from utils import make_one_concept
+import argparse
+import os
+import warnings
+
 emb_path = "data/word2vec_embeddings/glove.twitter.27B.200d.word2vec"
 embedding = KeyedVectors.load_word2vec_format(emb_path, binary=False)
 nlp = spacy.load("en_core_web_md")
@@ -55,11 +58,64 @@ def score_tokens(document, concepts, embedding):
     return scores
 
 
-def predict_df(df, text_col):
+def predict_df(df, text_col, output_path):
     df[[f"{f}_score" for f in concepts.keys()]] = 0
     for i, row in tqdm(df.iterrows(), desc="Scored", dynamic_ncols=True,
                        unit=" examples", leave=False, total=len(df)):
         scores = score_tokens(row[text_col], concepts, embedding)
         for concept, score in scores.items():
             df.loc[i, f"{concept}_score"] = score
-    return df
+    df.to_csv(output_path)
+
+
+def parse_args():
+    parser = argparse.ArgumentParser("Score texts using embedding similarity.")
+    parser.add_argument("--data",
+                        type=str,
+                        help="Path to the data file (CSV).",
+                        required=True)
+    parser.add_argument("--text_col",
+                        type=str,
+                        help="Name of the column in the data file that contains the texts.",
+                        required=True)
+    parser.add_argument("--verbose",
+                        type=int,
+                        help="Whether to print messages (1) or not (0).",
+                        default=1)
+    parser.add_argument("--output",
+                        type=str,
+                        help="Path to the output file (CSV).",
+                        required=True)
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+    args = parse_args()
+    data_path = args.data
+    text_col = args.text_col
+    verbose = bool(args.verbose)
+    output_path = args.output
+
+    # Check if the data file exists
+    assert os.path.exists(data_path), f"Data file does not exist at {data_path}."
+
+    # Check if the output file exists
+    if os.path.exists(output_path):
+        warnings.warn(f"Output file already exists at {output_path}. It will be overwritten.")
+
+    # Load the texts
+    if verbose:
+        print(f"Loading data from {data_path}...")
+    df = pd.read_csv(data_path)
+
+    # Score the texts
+    if verbose:
+        print(f"Scoring texts...")
+
+    predict_df(df=df,
+               text_col=text_col,
+               output_path=output_path
+               )
+
+    if verbose:
+        print("Done!")
