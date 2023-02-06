9d5d4defa1d65befcaa2dc1afcc7b8179a7d1e39
Josh Nguyen <tuandungnguyen99@gmail.com>
2023-02-06 06:05:21 +0000

    Add inverse calibration function

--- scripts/utils/calibration.py
+++ scripts/utils/calibration.py
@@ -37,3 +37,30 @@ def calibrate_saerens(y_test_score, y_train):
         y_test_score / (y_test_score + neg_to_pos_prop * (1 - y_test_score))
 
     return y_test_score_calibrated
+
+
+def inverse_calibrate_saerens(y_test_score_calibrated, y_train):
+    """
+    Apply the inverse of the Saerens calibration function.
+
+    Args:
+        y_test_score_calibrated: calibrated scores for test examples.
+                                 Array of float.
+        y_train: binary labels for training examples. Array of {0, 1}.
+
+    Returns:
+        The original scores for test examples. Array or float, depending on
+        the type of y_test_score_calibrated.
+    """
+
+    # Proportion of positive examples in the training set
+    train_pos_prop = np.mean(y_train)
+
+    # Ratio between negative and positive examples in the training set
+    neg_to_pos_prop = (1 - train_pos_prop) / train_pos_prop
+
+    # Apply inverse calibration
+    y_test_score = y_test_score_calibrated * neg_to_pos_prop / \
+        (1 - y_test_score_calibrated + y_test_score_calibrated * neg_to_pos_prop)
+
+    return y_test_score
